978cea23761e495716874513ab3d7d846a807202
Developer Shohan <dev.shohanur@gmail.com>
2024-03-04 00:04:24 +0600

    login reg api added

--- routers/cartItem.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authenticate from "../middleware/authenticate.js";
-import cartItemController from "../controllers/cartItem.controller.js";
-const router= express.Router();
-
-router.patch("/:id", authenticate, cartItemController.updateCartItem)
-router.delete("/:id", authenticate, cartItemController.removeCartItem)
-
-export default router
--- routers/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllUsers, getUserProfile } from '../controllers/user.controller.js';
-
-const router = express.Router();
-
-router.get('/profile', getUserProfile)
-router.get("/", getAllUsers)
-
-export default router;
